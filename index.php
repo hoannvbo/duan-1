5c18ded55661ea6ff0f3af2a223a7c5bfcbf243f
hoannvbo <150426245+hoannvbo@users.noreply.github.com>
2023-11-17 15:44:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web bán thức ăn </title>
+    <link rel="stylesheet" href="./index.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <a href="" class="logo">
+                <img src="assets/logo.png" alt="">
+            </a>
+            <div id="menu">
+                <div class="item">
+                    <a href="./index.php">Trang chủ</a>
+                </div>
+                <div class="item">
+                    <a href="./sanpham//sanpham.php">Sản phẩm</a>
+                </div>
+                <div class="item">
+                    <a href="./blog//blog.php">Blog</a>
+                </div>
+                <div class="item">
+                    <a href="./lienhe//lienhe.php">Liên hệ</a>
+                </div>
+            </div>
+            <div id="actions">
+                <div class="item">
+                    <a href="./login//login.php">
+                        <img src="assets/user.png" alt="">
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="./giohang//giohang.php">
+                        <img src="assets/cart.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div id="banner">
+            <div class="box-left">
+                <h2>
+                    <span>THỨC ĂN</span>
+                    <br>
+                    <span>THƯỢNG HẠNG</span>
+                </h2>
+                <p>Chuyên cung cấp các món ăn đảm bảo dinh dưỡng
+                    hợp vệ sinh đến người dùng,phục vụ người dùng 1 cái
+                    hoàn hảo nhất</p>
+                <button>Mua ngay</button>
+            </div>
+            <div class="box-right">
+                <img src="assets/img_1.png" alt="">
+                <img src="assets/img_2.png" alt="">
+                <img src="assets/img_3.png" alt="">
+            </div>
+            <div class="to-bottom">
+                <a href="">
+                    <img src="assets/to_bottom.png" alt="">
+                </a>
+            </div>
+        </div>
+        <div id="wp-products">
+            <h2>SẢN PHẨM CỦA CHÚNG TÔI</h2>
+            <ul id="list-products">
+                    <a href="/bundau/bundau.html">
+                        <div class="item">
+                            <img src="assets/bun.png" alt="">
+                            <div class="stars">
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                            </div>
+        
+                            <div class="name">Bún Đậu</div>
+                            <!-- <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                            <div class="price">500.000 VNĐ</div>-->
+                            </div>
+                    </a>
+                    <a href="/pho/pho.html">
+                        <div class="item">
+                            <img src="assets/pho.png" alt="">
+                            <div class="stars">
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                            </div>
+        
+                            <div class="name">Phở Bò</div>
+                            <!-- <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                            <div class="price">500.000 VNĐ</div> -->
+                        </div>
+                    </a>
+
+
+                    <a href="/goicuon/goicuon.html">
+                        <div class="item">
+                            <img src="assets/goi.png" alt="">
+                            <div class="stars">
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                            </div>
+        
+                            <div class="name">Gỏi Cuốn</div>
+                            <!-- <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                            <div class="price">500.000 VNĐ</div> -->
+                        </div>
+                    </a>
+
+                    <a href="/nemnuong/nemnuong.html">
+                        <div class="item">
+                            <img src="assets/nem.png" alt="">
+                            <div class="stars">
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                            </div>
+        
+                            <div class="name">Nem Nướng</div>
+                            <!-- <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                            <div class="price">500.000 VNĐ</div> -->
+                        </div>
+        
+                    </a>
+                    <a href="/banhxeo/banhxeo.html">
+                        <div class="item">
+                            <img src="assets/xeo.png" alt="">
+                            <div class="stars">
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                            </div>
+        
+                            <div class="name">Bánh Xèo</div>
+                            <!-- <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                            <div class="price">500.000 VNĐ</div> -->
+                        </div>
+                    </a>
+
+                    <a href="/banhcuon/banhcuon.html">
+                        <div class="item">
+                            <img src="assets/cuon.png" alt="">
+                            <div class="stars">
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                                <span>
+                                    <img src="assets/star.png" alt="">
+                                </span>
+                            </div>
+        
+                            <div class="name">Bánh Cuốn</div>
+                            <!-- <div class="desc">Mô Tả Ngắn Cho Sản Phẩm</div>
+                            <div class="price">500.000 VNĐ</div> -->
+                        </div>
+                    </a>
+            </ul>
+            <div class="list-page">
+                <div class="item">
+                    <a href="">1</a>
+                </div>
+                <div class="item">
+                    <a href="">2</a>
+                </div>
+                <div class="item">
+                    <a href="">3</a>
+                </div>
+                <div class="item">
+                    <a href="">4</a>
+                </div>
+            </div>
+        </div>
+
+        <div id="saleoff">
+            <div class="box-left">
+                <h1>
+                    <span>GIẢM GIÁ LÊN ĐẾN</span>
+                    <span>45%</span>
+                </h1>
+            </div>
+            <div class="box-right"></div>
+        </div>
+
+        <div id="comment">
+            <h2>NHẬN XÉT CỦA KHÁCH HÀNG</h2>
+            <div id="comment-body">
+                <div class="prev">
+                    <a href="#">
+                        <img src="assets/prev.png" alt="">
+                    </a>
+                </div>
+                <ul id="list-comment">
+                    <li class="item">
+                        <div class="avatar">
+                            <img src="assets/avatar_1.png" alt="">
+
+                        </div>
+                        <div class="stars">
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                        </div>
+                        <div class="name">Nguyễn Đình Vũ</div>
+
+                        <div class="text">
+                            <p>Lorem Ipsum is simply dummy text of the printing and
+                                typesetting industry. Lorem Ipsum has been the industry's
+                                standard dummy text ever since the 1500s, when an unknown
+                                printer took a galley of type and scrambled it to make a type
+                                specimen book.</p>
+                        </div>
+                    </li>
+                    <li class="item">
+                        <div class="avatar">
+                            <img src="assets/avatar_1.png" alt="">
+
+                        </div>
+                        <div class="stars">
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                        </div>
+                        <div class="name">Trần Ngọc Sơn</div>
+
+                        <div class="text">
+                            <p>Lorem Ipsum is simply dummy text of the printing and
+                                typesetting industry. Lorem Ipsum has been the industry's
+                                standard dummy text ever since the 1500s, when an unknown
+                                printer took a galley of type and scrambled it to make a type
+                                specimen book.</p>
+                        </div>
+                    </li>
+                    <li class="item">
+                        <div class="avatar">
+                            <img src="assets/avatar_1.png" alt="">
+
+                        </div>
+                        <div class="stars">
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                            <span>
+                                <img src="assets/star.png" alt="">
+                            </span>
+                        </div>
+                        <div class="name">Nguyễn Trần Vi</div>
+
+                        <div class="text">
+                            <p>Lorem Ipsum is simply dummy text of the printing and
+                                typesetting industry. Lorem Ipsum has been the industry's
+                                standard dummy text ever since the 1500s, when an unknown
+                                printer took a galley of type and scrambled it to make a type
+                                specimen book.</p>
+                        </div>
+                    </li>
+                </ul>
+                <div class="next">
+                    <a href="#">
+                        <img src="assets/next.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+        <div id="footer">
+            <div class="box">
+                <div class="logo">
+                    <img src="assets/logo.png" alt="">
+                </div>
+                <p>Cung cấp sản phẩm với chất lượng an toàn cho quý khách</p>
+            </div>
+            <div class="box">
+                <h3>NỘI DUNG</h3>
+                <ul class="quick-menu">
+                    <div class="item">
+                        <a href="./index.js">Trang chủ</a>
+                    </div>
+                    <div class="item">
+                        <a href="./sanpham//sanpham.php">Sản phẩm</a>
+                    </div>
+                    <div class="item">
+                        <a href="/blog//blog.php">Blog</a>
+                    </div>
+                    <div class="item">
+                        <a href="/lienhe//lienhe.php">Liên hệ</a>
+                    </div>
+                </ul>
+            </div>
+            <div class="box">
+                <h3>LIÊN HỆ</h3>
+                <form action="">
+                    <input type="text" placeholder="Phone">
+                    <button>Nhận tin</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="./index.js"></script>
+</body>
+
+</html>
